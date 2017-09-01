6e4372796af61e548623737542a539c86598e487
vkowsii <30829409+vkowsii@users.noreply.github.com>
2017-09-01 14:57:21 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char c;
+print("enter the alphabet");
+scanf("%d",c);
+{
+printf("the value of c is vowles");
+else
+{
+printf("the value of c is constant");
+}
+}
