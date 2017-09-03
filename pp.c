f90104d89b310fd3e4f37e840aa38f4a7d6de84e
vkowsii <30829409+vkowsii@users.noreply.github.com>
2017-09-03 22:03:34 +0530

    Create pp.c

---
+++ pp.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int  main()
+inta[ 1000];b[1000];
+printf( "enter the value");
+gets( a)
+strcpy( b,a)
+strrev( b)
+if( strcmp(a,b))
+printf("the value is palindrome ");
+else 
+printf( "the value is not palindrome")
+} 
+} 
