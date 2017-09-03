eed9e81a9e47df9bc30e39b65260ad7a98a21607
vkowsii <30829409+vkowsii@users.noreply.github.com>
2017-09-03 21:42:27 +0530

    Create powr.c

---
+++ powr.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int  main()
+{ 
+int  base;
+int  exponent;
+print( "enter the value");
+scanf( "%d",&base);
+print( "entet the value");
+scanf( "%d",&exponent);
+while( exponent !=1)
+{ 
+result= base;
++++exponent;
+} 
+printf( "the answer is);
+} 
+} 
