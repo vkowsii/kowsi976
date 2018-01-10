8d9f25c6c00305dec68f4104872e4b1ed2b88cb8
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-01-10 10:37:32 +0000

    Create timemin.c

---
+++ timemin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char datestr;
+char timestr;
+{
+strdate(strdate);
+printf("enter the current is %d /n date");
+strtime(strtime);
+printf("enter the current is %d /n time");
+}
+}
