27a94df30c3678aacdbb549a0c1de32371303879
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-01-31 02:28:08 -0800

    Rename swap2.c to swapnum.c

---
+++ swapnum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+double first num,second num;
+printf("enter the  first number ");
+scanf("%d",&f);
+printf("enter the second number");
+scanf("%d",&f);
+tempory variable= first number;
+second number=first  number;
+{
+printf("enter the swap first number");
+printF("enter the swap second number");
+}
+}
