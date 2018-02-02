4c1a0f98ee8f19042ed3e6a70e7cbd02fa8e4bf8
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-02-02 14:25:18 +0530

    Create strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,15 @@
+#iclude<stdio.h>
+int main()
+{
+int arr1=[helloworld];
+ arr2=[helloworld];
+printf("enter the greatest string");
+strleng=n;
+strleng=k;
+ if(n>k)
+print("the greatest string");
+else
+{
+if(n<k)
+printf("the greatest string is");
+}
